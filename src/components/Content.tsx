1f99fe7ed827849d1fc807c6444af42db89505b5
Wilson Franca <wilson.franca.92@gmail.com>
2021-03-15 23:18:48 -0700

    refactor: added extracted components and improved responsiveness.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,51 @@
-export function Content() {
-  // Complete aqui
+import React, { ReactNode, useEffect, useState } from 'react';
+import { api } from '../services/api';
+import { GenreResponseProps } from './SideBar';
+import { MovieCard } from './MovieCard';
+import '../styles/content.scss';
+import Header from './Header';
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
 }
+
+interface ContentProps {
+  selectedGenreId: number;
+}
+
+const Content: React.FunctionComponent<ContentProps> = ({ selectedGenreId }) => {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  return (
+    <div className="container">
+      <Header title={selectedGenre.title} />
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Content;
